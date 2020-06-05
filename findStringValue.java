17537d4cce32510094b82f392c482ba3224cc60b
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-06-06 05:20:10 +0530

    Add files via upload

---
+++ findStringValue.java
@@ -0,0 +1,55 @@
+package com.wipro.practice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class findStringValue {
+    static int findStringCode(String in) {
+        in = in.toUpperCase();
+        int val;
+        String concat = "";
+
+        for (String split : in.split(" ")) {
+            int len = split.length();
+            int sum = 0;
+            if (split.length() % 2 == 1) {
+                for (int i = 0; i <= split.length() / 2; i++) {
+                    val = Math.abs(split.charAt(i) - 'A' + 1) - Math.abs(split.charAt(len - 1) - 'A' + 1);
+                    sum += (Math.abs(val));
+                    len--;
+                    if (i == len) {
+                        val = Math.abs(split.charAt(i) - 'A' + 1);
+                        sum += val;
+                    }
+
+                }
+            } else {
+                for (int i = 0; i < split.length() / 2; i++) {
+                    val = Math.abs(split.charAt(i) - 'A' + 1) - Math.abs(split.charAt(len - 1) - 'A' + 1);
+                    sum += (Math.abs(val));
+                    len--;
+                    if (i == len) {
+                        val = Math.abs(split.charAt(i) - 'A' + 1);
+                        sum += val;
+                    }
+
+                }
+            }
+            System.out.println(sum);
+            concat += sum;
+
+        }
+        System.out.println(concat);
+        return 0;
+    }
+
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        String in = sc.nextLine();
+        findStringCode(in);
+    }
+
+
+}
