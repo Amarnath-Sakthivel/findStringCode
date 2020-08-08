8a9b12aed71d8ac80618fa38b6d7bb6734d861cb
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-08-08 19:55:08 +0530

    Update findStringValue.java

--- findStringValue.java
+++ findStringValue.java
@@ -2,49 +2,25 @@ package com.wipro.practice;
 import java.util.Scanner;
 
 public class findStringValue {
-    static int findStringCode(String in) {
-        in = in.toUpperCase();
-        int val;
-        String concat = "";
-        for (String split : in.split(" ")) {
-            int len = split.length();
-            int sum = 0;
-            if (split.length() % 2 == 1) {
-                for (int i = 0; i <= split.length() / 2; i++) {
-                    val = Math.abs(split.charAt(i) - 'A' + 1) - Math.abs(split.charAt(len - 1) - 'A' + 1);
-                    sum += (Math.abs(val));
-                    len--;
-                    if (i == len) {
-                        val = Math.abs(split.charAt(i) - 'A' + 1);
-                        sum += val;
-                    }
-                }
-            } else {
-                for (int i = 0; i < split.length() / 2; i++) {
-                    val = Math.abs(split.charAt(i) - 'A' + 1) - Math.abs(split.charAt(len - 1) - 'A' + 1);
-                    sum += (Math.abs(val));
-                    len--;
-                    if (i == len) {
-                        val = Math.abs(split.charAt(i) - 'A' + 1);
-                        sum += val;
-                    }
-                }
-            }
-            System.out.println(sum);
-            concat += sum;
-
-        }
-        System.out.println(concat);
-        return 0;
-    }
-
-
-    public static void main(String args[]) {
-
-        Scanner sc = new Scanner(System.in);
-        String in = sc.nextLine();
-        findStringCode(in);
-    }
-
-
+  static int findCodeValue(String input1) {
+		input1 = input1.toUpperCase();
+		String finalResult = "";
+		for (String split : input1.split(" ")) {
+			int sum = 0;
+			if (split.length() % 2 == 1) {
+				for (int i = 0; i <= split.length() / 2; i++) {
+					sum += (split.charAt(i) == split.charAt(split.length() - i - 1))
+							? Math.abs(split.charAt(i) - 'A' + 1)
+							: Math.abs((split.charAt(i) - 'A' + 1) - (split.charAt(split.length() - i - 1) - 'A' + 1));
+				}
+			} else if (split.length() % 2 == 0) {
+				for (int i = 0; i < split.length() / 2; i++) {
+					sum += Math.abs((split.charAt(i) - 'A' + 1) - (split.charAt(split.length() - i - 1) - 'A' + 1));
+				}
+			}
+			finalResult += sum;
+		}
+		//System.out.println(finalResult);
+		return finalResult;
+	}
 }
